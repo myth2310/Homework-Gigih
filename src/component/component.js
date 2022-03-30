7e7083375eea9550f8788b2c547193a302f4018b
myth2310 <achmadmiftahudin2310@gmail.com>
2022-03-30 07:52:27 +0700

    Homework Gigih

---
+++ src/component/component.js
@@ -0,0 +1,43 @@
+import '../../src/App.css';
+// import Home from './pages/home/indexx';
+import Data from './data';
+
+  
+function Card() {
+    return (
+        <div className="App">
+            <header className="App-header">
+          <div className='search'>
+              <input placeholder='Search...'></input>
+              <button>Search</button>
+         </div>
+         
+         {Data.map((data) => (  
+        <div className="playlist-container">
+          <div className="playlist-item">
+            <img
+              className="playlist-image"
+              src={data.album.images[0].url}
+              alt="playlist"
+            />
+            <div className="playlist-content">
+              <h2 className="playlist-title">{data.album.name}</h2>
+              <div className='desc'>
+              <p>{data.artists[0].name}</p>
+              <p>{data.album.total_tracks} Songs</p>
+              </div>
+            </div>
+            <div className="playlist-actions">
+              <button className="playlist-action" id=''><a href={data.external_urls.spotify}>Select</a></button>
+            </div>
+          </div>
+        </div>
+       ))}  
+
+        
+         </header>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/pages/home/indexx.js
@@ -0,0 +1,16 @@
+import { hover } from '@testing-library/user-event/dist/hover';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <img
+            src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif"
+            alt=""
+        />
+    );
+};
+
+
+
+
+export default Home;
